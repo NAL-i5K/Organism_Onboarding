1838a41cb01e244d2feab72df07e3d14e9930a14
Chun-Hung <32384566+r06942072@users.noreply.github.com>
2019-07-30 14:38:47 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,27 +1,31 @@
 #!/bin/sh
 
-#Assuming that data wrangler has two things already install
+#Assuming two things already install
 #1. python 
 #2. pip
 
 echo "[Step1.] Clone all the remote repos to local"
-#git clone https://github.com/r06942072/pro_emoji.git
+#apollo2_data_processing needed
+#faToTwoBit
+#samtools
+#jbrowse, https://github.com/GMOD/jbrowse
+#wiggle-tool
+
+#django needed
+#git clone https://github.com/NAL-i5K/genomics-workspace.git
 
 echo
-echo "[Step2.] Install pipenv for python environment"
+echo "[Step2.] Setup python environment"
 #PYTHON=$(which python)
 pip install pipenv
 
-echo 
-echo "[Step3.] Locate the Pipfile, create a new virtual environment and install the necessary packages."
-pipenv install
-#django
+#Locate the Pipfile, create a new virtual environment and install the necessary packages.
+pipenv install  
 
-echo 
-echo "[Step4.] Activate the virtual environment"
+# Activate the virtual environment"
 pipenv shell
 
 echo 
-echo "[Finish] Let's start editting job-[gggsss].yml before we run cwl-runner"
+echo "[Finish] Let's start editting job-[gggsss].yml before running " ```cwl-runner```
 
 
